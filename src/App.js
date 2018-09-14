da83f178912be5e61b55a41db7c0805f3b84bf09
Jørgen Buholm Johansen <jorgenbj@stud.ntnu.no>
2018-09-14 10:22:42 +0200

    Har laget en bildekomponent med axios rammeverket, dog den fungerer ikke enda

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React, { Component } from 'react';
-import car from './SVGPictures/Cars/Gallardo.svg';
+import Picture from './Components/Picture';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={car} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Picture />
     );
   }
 }
