8135ba0fe945a016a29fdba61042f2ff7702bf93
Jørgen Buholm Johansen <jorgenbj@stud.ntnu.no>
2018-09-17 10:45:18 +0200

    #15 har fikset bug med å laste inn innhold med dangerouslySetInnerHTML og ordnet høyde og bredde på bildene

--- src/Components/Picture.js
+++ src/Components/Picture.js
@@ -7,16 +7,27 @@ class Picture extends Component {
     }
     
     componentDidMount(){
-        axios.get(`../SVGPictures/Animals/Bat.svg`)
+        axios.get(`SVGPictures/Animals/Wolf.svg`)
             .then(res => {
+                console.log(res.data);
+                //const test = React.createElement(res.data);
                 this.setState({picture: res.data});
             })
-        }
+            .catch( (e) => 
+                {
+                this.setState( (state) => {
+                    return {picture: 'We could not display picture'};
+            })
+        })
+    }
+
 
     render(){
         return(
-        <img src={this.state.picture} className="picture" alt='toBadBruh' />
-        );
+            <div >
+            <div className="picture" dangerouslySetInnerHTML={{__html: this.state.picture}} />
+            </div>
+    );
     }
 }
 
