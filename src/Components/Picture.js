cc53b90046ec4a2aa436f77ac227744945ad074d
Jørgen Buholm Johansen <jorgenbj@stud.ntnu.no>
2018-09-17 11:40:46 +0200

    #15 har lagt til lister for bildene og henter de inn med indeks

--- src/Components/Picture.js
+++ src/Components/Picture.js
@@ -3,11 +3,14 @@ import axios from 'axios';
 
 class Picture extends Component {
     state = {
-        picture: ''
+        picture: '',
+        animalList: ["SVGPictures/Animals/Animal1.svg","SVGPictures/Animals/Animal2.svg","SVGPictures/Animals/Animal3.svg"],
+        carList: ["SVGPictures/Cars/Car1.svg","SVGPictures/Cars/Car2.svg","SVGPictures/Cars/Car3.svg","SVGPictures/Cars/Car4.svg"],
+        flowerList: ["SVGPictures/Flowers/Flower1.svg","SVGPictures/Flowers/Flower2.svg","SVGPictures/Flowers/Flower3.svg","SVGPictures/Flowers/Flower4.svg"]
     }
     
     componentDidMount(){
-        axios.get(`SVGPictures/Animals/Wolf.svg`)
+        axios.get(this.state.animalList[0])
             .then(res => {
                 console.log(res.data);
                 //const test = React.createElement(res.data);
