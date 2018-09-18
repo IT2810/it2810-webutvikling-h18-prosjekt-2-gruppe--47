48fe8f260b0807208e47d76dac7715978007d2bd
Jørgen Buholm Johansen <jorgenbj@stud.ntnu.no>
2018-09-18 20:23:01 +0200

    Har lagt til switch statement for valg av bilde og satt inn linkene i en to-dimensjonal liste

--- src/Components/Picture.js
+++ src/Components/Picture.js
@@ -1,18 +1,77 @@
 import React, {Component} from 'react';
 import axios from 'axios';
-
+//import Tabs from "./Tabs";
+//import Categories from "./Categories";
 class Picture extends Component {
     state = {
         picture: '',
-        animalList: ["SVGPictures/Animals/Animal1.svg","SVGPictures/Animals/Animal2.svg","SVGPictures/Animals/Animal3.svg"],
-        carList: ["SVGPictures/Cars/Car1.svg","SVGPictures/Cars/Car2.svg","SVGPictures/Cars/Car3.svg","SVGPictures/Cars/Car4.svg"],
-        flowerList: ["SVGPictures/Flowers/Flower1.svg","SVGPictures/Flowers/Flower2.svg","SVGPictures/Flowers/Flower3.svg","SVGPictures/Flowers/Flower4.svg"]
+        allPictures: [["SVGPictures/Animals/Animal1.svg","SVGPictures/Animals/Animal2.svg","SVGPictures/Animals/Animal3.svg","SVGPictures/Animals/Animal4.svg"],
+                    ["SVGPictures/Cars/Car1.svg","SVGPictures/Cars/Car2.svg","SVGPictures/Cars/Car3.svg","SVGPictures/Cars/Car4.svg"],
+                    ["SVGPictures/Flowers/Flower1.svg","SVGPictures/Flowers/Flower2.svg","SVGPictures/Flowers/Flower3.svg","SVGPictures/Flowers/Flower4.svg"]]
     }
+
+    choosePicture = (command) =>{
+        let category;
+        let picture;
+        switch(command){
+            case 0:
+                category = 0;
+                picture = 0;
+                break;
+            case 1:
+                category = 0;
+                picture = 1;
+                break;
+            case 2:
+                category = 0;
+                picture = 2;
+                break;
+            case 3:
+                category = 0;
+                picture = 3;
+                break;
+            case 4:
+                category = 1;
+                picture = 0;
+                break;
+            case 5:
+                category= 1;
+                picture = 1;
+                break;
+            case 6:
+                category = 1;
+                picture = 2;
+                break;
+            case 7:
+                category = 1;
+                picture = 3;
+                break;
+            case 8:
+                category = 2;
+                picture = 0;
+                break;
+            case 9:
+                category = 2;
+                picture = 1;
+                break;
+            case 10:
+                category = 2;
+                picture = 2;
+                break;
+            default:
+                category = 2;
+                picture = 3;
+        }
+        return [category, picture];
+    }
+
     
     componentDidMount(){
-        axios.get(this.state.animalList[0])
+        let list = this.choosePicture(11);
+        console.log(list);
+        axios.get(this.state.allPictures[list[0]][list[1]])
             .then(res => {
-                console.log(res.data);
+                //console.log(res.data);
                 //const test = React.createElement(res.data);
                 this.setState({picture: res.data});
             })
