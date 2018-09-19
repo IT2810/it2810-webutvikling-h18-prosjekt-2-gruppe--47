f4a1944884a37517654f95abb5937b2574217f22
Sigurd Øines <sigurdoines@gmail.com>
2018-09-19 10:05:36 +0200

    feat: set up basis for component communication through App.js. #22

--- src/App.js
+++ src/App.js
@@ -4,20 +4,47 @@ import './App.css';
 import ResourceComponent from './ResourceComponent'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-          <ResourceComponent imageCategory={2} textCategory={1} exhibition={2}></ResourceComponent>
-      </div>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            imageCategory: 0,
+            textCategory: 0,
+            soundCategory: 0,
+            exhibition: 0
+        };
+
+        this.handleCategoryChange = this.handleCategoryChange.bind(this);
+        this.handleTabChange = this.handleTabChange.bind(this);
+    }
+
+    handleTabChange(new_value) {
+        this.setState({
+            exhibition: new_value
+        })
+    }
+
+    handleCategoryChange(new_values){
+        this.setState({
+            imageCategory: new_values.imageCategory,
+            textCategory: new_values.textCategory,
+            soundCategory: new_values.soundCategory
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+                <ResourceComponent imageCategory={this.state.imageCategory} textCategory={this.state.textCategory} exhibition={this.state.exhibition}></ResourceComponent>
+            </div>
+        );
+    }
 }
 
 export default App;
