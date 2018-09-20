ce4342765076c08fb0a39a9c0fd70dd973a83921
Sigurd Øines <sigurdoines@gmail.com>
2018-09-20 12:38:58 +0200

    fix: Oppdaterte SoundPlayer slik at den reloader audio når den får ny source. #17

--- src/Components/SoundPlayer.js
+++ src/Components/SoundPlayer.js
@@ -3,12 +3,21 @@ import React, {Component} from 'react';
 class SoundPlayer extends Component {
     constructor(props){
         super(props);
+        this.audioRef = React.createRef();
+    }
+
+    componentDidUpdate(prevProps) {
+        if(prevProps.soundSource !== this.props.soundSource) {
+            this.audioRef.current.pause();
+            this.audioRef.current.load();
+            this.audioRef.current.play();
+        }
     }
 
     render(){
         return(
             <div>
-            <audio controls>
+            <audio controls ref={this.audioRef}>
                 <source src = {this.props.soundSource} type="audio/mp3"/>
                 Your browser does not support this audio tag
             </audio>
